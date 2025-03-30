4259f504e555d40c27cc98bbbec2272d8c4be1f6
Amampel <adriaml1998@gmail.com>
2025-03-31 00:59:04 +0200

    wip: esto es la primera prueba

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen text-center">
+    <h1 class="text-3xl font-bold mb-4">Aplicación EIMEC</h1>
+    <img src="../assets/EIMEC-FORMACION-PRACTICA-MEDICINA-ESTETICA.png" />
+  </div>
+</template>
+
+<script setup>
+// No hay lógica aún
+</script>
